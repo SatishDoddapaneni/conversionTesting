25d28d9131759ddf548e2fa3f0e0f11b3b35b065
SatishDoddapaneni <64103661+SatishDoddapaneni@users.noreply.github.com>
2020-04-22 07:32:50 +0200

    Add files via upload

---
+++ KelvinCelsius/src/main/java/com/KelvinCelsius/KelvinCelsius/KelvinCelsiusApplication.java
@@ -0,0 +1,13 @@
+package com.KelvinCelsius.KelvinCelsius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelvinCelsiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelvinCelsiusApplication.class, args);
+	}
+
+}
---
+++ KelvinCelsius/src/test/java/com/KelvinCelsius/KelvinCelsius/KelvinCelsiusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KelvinCelsius.KelvinCelsius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelvinCelsiusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
